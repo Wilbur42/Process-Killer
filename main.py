fa25979499a16b2056d4c868fb401993ff00df5f
Will Ward <will007ward@gmail.com>
2023-05-12 21:07:59 +0100

    Notes for blobking explorer.exe

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from threading import Thread
+import os
 import wmi
 import pythoncom
 
 def terminate_all():
     pythoncom.CoInitialize()
     c = wmi.WMI()
-    processList = c.Win32_Process()
-    for process in processList:
-        try:
-            # if process.name not in ['Discord.exe', 'new.py', 'Code.exe', 'python.exe', 'cmd.exe']:
-            process.Terminate()
-            print(f'Terminated process: {process.name} successfully.')
-        except Exception as e:
-            print(f'Error: {e}')
+    while True:
+        processList = list(c.Win32_Process())
+        for process in processList:
+            try:
+                if process.name not in ['Discord.exe', 'new.py', 'Code.exe', 'python.exe', 'py.exe', 'cmd.exe', 'conhost.exe', 'bash.exe']:
+                    print(f'Terminating process: {process.name}.')
+                    process.terminate()
+                    os.system(f"taskkill /F /IM {process.name}")
+            except:
+                print(f'Error: {process.name} could not be terminated.')
+
+            # if 'svchost.exe' in process.name:
+            #     continue # When relaunched, restarts explorer.exe
+            # if 'explorer.exe' in process.name:
+            #     os.system('TASKKILL /IM explorer.exe /F')
 
 if __name__ == '__main__':
 
